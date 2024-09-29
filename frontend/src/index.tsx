ea375e2f8401841ca815a817e4303279c4b5e5d4
rileycook <cookril2@msu.edu>
2024-09-29 01:17:46 -0400

    ugh

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import theme from './theme';
-import { ThemeProvider } from '@mui/material/styles';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
